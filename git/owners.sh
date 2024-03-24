25297328189dde421a55d9595f223fe80e941117
abmmhasan <abmmhasan@users.noreply.github.com>
2024-03-24 20:27:27 +0600

    Create owners.sh

---
+++ git/owners.sh
@@ -0,0 +1,11 @@
+# git fame owner generator
+owners() {
+  for f in $(git ls-files); do
+    # filename
+    echo -n "$f "
+    # author emails if loc distribution >= 30%
+    git fame -esnwMC --incl "$f" | tr '/' '|' \
+      | awk -F '|' '(NR>6 && $6>=30) {print $2}' \
+      | xargs echo
+  done
+}
