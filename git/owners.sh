4f40ccb1eb7e3547b24f6fe87f4c777cadf24647
abmmhasan <abmmhasan@users.noreply.github.com>
2024-03-25 19:21:11 +0600

    Update owners.sh

--- git/owners.sh
+++ git/owners.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 # git fame owner generator
-owners() {
-  for f in $(git ls-files); do
-    # filename
-    echo -n "$f "
-    # author emails if loc distribution >= 30%
-    git fame -esnwMC --incl "$f" | tr '/' '|' \
-      | awk -F '|' '(NR>6 && $6>=30) {print $2}' \
-      | xargs echo
-  done
-}
+for f in $(git ls-files); do
+  # filename
+  echo -n "$f "
+  # author emails if loc distribution >= 30%
+  git fame -esnwMC --incl "$f" | tr '/' '|' \
+    | awk -F '|' '(NR>6 && $6>=30) {print $2}' \
+    | xargs echo
+done
